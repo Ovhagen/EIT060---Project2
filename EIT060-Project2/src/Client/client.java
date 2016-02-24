cbd6ca3ddd797b4aea8d77e900bf317f7933b8a8
Christoffer <Christoffer@Christoffers>
2016-02-24 15:13:51 +0100

    La till quitcommand vid inlogg av Client

--- EIT060-Project2/src/Client/client.java
+++ EIT060-Project2/src/Client/client.java
@@ -19,6 +19,7 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 import javax.security.cert.X509Certificate;
 
+
 import Server.server;
 
 /*
@@ -50,15 +51,22 @@ public class client {
 		loc = loc.substring(5, loc.length() - 5);
 		SSLSocketFactory factory = null;
 		Console console = System.console();
+		boolean terminate = false;
 		
 		while (login) {
+			System.out.println("Type \"quit\" to exit program");
 			try {
 				System.out.println("UserID: ");
 				userPath = scan.nextLine();
-	
+				if(userPath.equalsIgnoreCase("quit")){
+					login = false;
+					scan.close();
+					System.out.println("Quit program");
+					terminate = true;
+					break;
+				}
 				/*Users keystore password is entered*/
-				char[] password = console.readPassword("Password: ");
-				passwd = new String(password);
+				char[] pass = console.readPassword("Password: ");
 				
 				
 				keyfile = new FileInputStream(loc + "/certificates/Users/" + userPath + "/" + userPath + "_keystore");
@@ -66,7 +74,6 @@ public class client {
 				
 				//SSLSocketFactory factory = null;
 				try {
-					char[] pass = passwd.toCharArray();
 					
 					KeyStore ks = KeyStore.getInstance("JKS");
 					KeyStore ts = KeyStore.getInstance("JKS");
@@ -99,86 +106,87 @@ public class client {
 				System.out.println("Username does not exist. Please try again.");
 			}
 		}
-
-		try { /* set up a key manager for client authentication */
-			
-			
-			SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
-			System.out.println("\nsocket before handshake:\n" + socket + "\n");
-
-			/*
-			 * send http request
-			 * 
-			 * See SSLSocketClient.java for more information about why there is
-			 * a forced handshake here when using PrintWriters.
-			 */
-			socket.startHandshake();
-
-			SSLSession session = socket.getSession();
-			X509Certificate cert = (X509Certificate) session.getPeerCertificateChain()[0];
-			String subject = cert.getSubjectDN().getName();
-
-			String issuer = cert.getIssuerDN().getName();
-			String serial = cert.getSerialNumber().toString();
-			
-			
-
-
-
-
-			BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String msg;
-			String allowed = in.readLine();
-			if(allowed.equalsIgnoreCase("notAllowed")){
-				throw new LoginException("User already logged in");
-			} 
-			
-			System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject
-					+ "\n" + "issuer name (issuer DN field) on certificate received from server:\n" + issuer);
-			System.out.println("Serial number: " + serial);
-			System.out.println("socket after handshake:\n" + socket + "\n");
-			System.out.println("secure connection established\n\n");
-			
-			System.out.println("Succesful login! Type a command in the prompt and press enter.");
-			System.out.println("Get -g, Put -p, Edit -e, Help -h, -r Remove, -pa Print All, Logout logout, Quit quit");
-			
-			for (;;) {
-				System.out.print(">");
-				msg = read.readLine();
-				if (msg.equalsIgnoreCase("quit") || msg.equalsIgnoreCase("logout")) {
-					break;
-				}
-				out.println(msg);
-				out.flush();
-				String fromServer;
-				while ((fromServer = in.readLine()) != null) {
-				    if (fromServer.equals("listen")){
-				        break;
-				    } else {
-					    System.out.println(fromServer);
-				    }
-				}
+		if(!terminate){
+			try { /* set up a key manager for client authentication */
 				
 				
-			}
-			if(msg.equals("logout")){
-				System.out.println("Logout out");
-				socket.close();
-				in.close();
-				out.close();
+				SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+				System.out.println("\nsocket before handshake:\n" + socket + "\n");
+	
+				/*
+				 * send http request
+				 * 
+				 * See SSLSocketClient.java for more information about why there is
+				 * a forced handshake here when using PrintWriters.
+				 */
+				socket.startHandshake();
+	
+				SSLSession session = socket.getSession();
+				X509Certificate cert = (X509Certificate) session.getPeerCertificateChain()[0];
+				String subject = cert.getSubjectDN().getName();
+	
+				String issuer = cert.getIssuerDN().getName();
+				String serial = cert.getSerialNumber().toString();
+				
+				
+	
+	
+	
+	
+				BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String msg;
+				String allowed = in.readLine();
+				if(allowed.equalsIgnoreCase("notAllowed")){
+					throw new LoginException("User already logged in");
+				} 
+				
+				System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject
+						+ "\n" + "issuer name (issuer DN field) on certificate received from server:\n" + issuer);
+				System.out.println("Serial number: " + serial);
+				System.out.println("socket after handshake:\n" + socket + "\n");
+				System.out.println("secure connection established\n\n");
+				
+				System.out.println("Succesful login! Type a command in the prompt and press enter.");
+				System.out.println("Get -g, Put -p, Edit -e, Help -h, -r Remove, -pa Print All, Logout logout, Quit quit");
+				
+				for (;;) {
+					System.out.print(">");
+					msg = read.readLine();
+					if (msg.equalsIgnoreCase("quit") || msg.equalsIgnoreCase("logout")) {
+						break;
+					}
+					out.println(msg);
+					out.flush();
+					String fromServer;
+					while ((fromServer = in.readLine()) != null) {
+					    if (fromServer.equals("listen")){
+					        break;
+					    } else {
+						    System.out.println(fromServer);
+					    }
+					}
+					
+					
+				}
+				if(msg.equals("logout")){
+					System.out.println("Logged out");
+					socket.close();
+					in.close();
+					out.close();
+					init();
+				} else {
+					System.out.println("Exit client");
+					socket.close();
+					in.close();
+					out.close();
+					read.close();
+				}
+			} catch (LoginException e) {
+				System.out.println(e.getMessage());
 				init();
-			} else {
-				System.out.println("Avslutade");
-				socket.close();
-				in.close();
-				out.close();
-				read.close();
 			}
-		} catch (LoginException e) {
-			System.out.println(e.getMessage());
-			init();
 		}
 	}
 	public class LoginException extends Exception {
--- EIT060-Project2/src/Server/Auditer.java
+++ EIT060-Project2/src/Server/Auditer.java
@@ -47,7 +47,7 @@ public class Auditer {
 	
 	synchronized private void print(String s){
 		try {
-			writer = new PrintWriter(new FileWriter(location + "/AuditLogs/" + todaysDateNoTime + "/" + todaysDateWithTime, true));
+			writer = new PrintWriter(new FileWriter(location + "/AuditLogs/" + todaysDateNoTime + "/Auditlog" + todaysDateWithTime, true));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
